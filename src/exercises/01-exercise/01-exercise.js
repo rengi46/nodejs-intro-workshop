be122873afc97763cb8e420f167efe0601108a11
Dani Lucaci <danilucaci@gmail.com>
2020-12-28 15:31:18 +0100

    Add 01-exercise solution

--- src/exercises/01-exercise/01-exercise.js
+++ src/exercises/01-exercise/01-exercise.js
@@ -2,6 +2,7 @@
  * 1. Import the `BASE_NUM` variable from the `constants.js` file
  *    and use it in the `add()` function to add it to the 2 parameters
  */
+const BASE_NUM = require("./constants");
 
 /**
  * 2. Use module.exports = { ... } to export 2 functions:
@@ -13,3 +14,11 @@
  * 2.2. subtract(a, b)
  *    Takes 2 parameters `a` and `b` and returns `b` - `a`
  */
+module.exports = {
+  add: function (a, b) {
+    return a + b + BASE_NUM;
+  },
+  subtract: function (a, b) {
+    return b - a;
+  },
+};
